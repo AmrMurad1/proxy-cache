998f77b0f6ade0c9f84639dacee761f2af23f1f9
Amr Murad <amrmurad2003@gmail.com>
2025-10-13 01:06:34 +0300

    entry point implemented

--- cmd/proxy-cache/main.go
+++ cmd/proxy-cache/main.go
@@ -1,4 +1,32 @@
-package proxycache
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
+	// TODO: clear cache
+	port := flag.Int("port", 0, "Port for the caching proxy server")
+	origin := flag.String("origin", "", "Origin server URL")
+
+	flag.Parse()
+
+	if *port == 0 || *origin == "" {
+		log.Fatal("Error: --port and --origin are required")
+	}
+
+	fmt.Printf("Starting proxy server on port %d\n", *port)
+	fmt.Printf("Forwarding to %s\n", *origin)
+
+	// Create http handler
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Received %s %s", r.Method, r.URL.Path)
+		fmt.Fprintf(w, "Hello from proxy!")
+	})
+
+	addr := fmt.Sprintf(":%d", *port)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
