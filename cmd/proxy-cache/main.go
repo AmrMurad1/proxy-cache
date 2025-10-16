ab11f1354cd353685da948b553e04d06cb55891d
Amr Murad <amrmurad2003@gmail.com>
2025-10-17 01:52:40 +0300

    entry point implemented

--- cmd/proxy-cache/main.go
+++ cmd/proxy-cache/main.go
@@ -5,28 +5,55 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/AmrMurad1/proxy-cache/internal"
 )
 
 func main() {
-	// TODO: clear cache
+	// Parse command-line flags
 	port := flag.Int("port", 0, "Port for the caching proxy server")
 	origin := flag.String("origin", "", "Origin server URL")
+	clearCache := flag.Bool("clear-cache", false, "Clear the cache")
 
 	flag.Parse()
 
-	if *port == 0 || *origin == "" {
-		log.Fatal("Error: --port and --origin are required")
+	// Handling --clear-cache flag
+	if *clearCache {
+		fmt.Println("Clearing cache...")
+		proxy := internal.NewProxy("")
+		proxy.ClearCache()
+		os.Exit(0)
 	}
 
-	fmt.Printf("Starting proxy server on port %d\n", *port)
-	fmt.Printf("Forwarding to %s\n", *origin)
+	// Validate required flags
+	if *port == 0 {
+		fmt.Println("Error: --port is required")
+		fmt.Println("\nUsage:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
-	// Create http handler
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Received %s %s", r.Method, r.URL.Path)
-		fmt.Fprintf(w, "Hello from proxy!")
-	})
+	if *origin == "" {
+		fmt.Println("Error: --origin is required")
+		fmt.Println("\nUsage:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
+	proxy := internal.NewProxy(*origin)
+
+	// Register proxy as HTTP handler
+	http.Handle("/", proxy)
+
+	// Start server
 	addr := fmt.Sprintf(":%d", *port)
-	log.Fatal(http.ListenAndServe(addr, nil))
+	fmt.Printf("Starting caching proxy server on port %d\n", *port)
+	fmt.Printf("Forwarding requests to %s\n", *origin)
+	fmt.Printf("Server listening on %s\n", addr)
+
+	// Start listening
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
 }
